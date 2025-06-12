7c78c168bed283af7be397d6c59cfd0289be70bb
Hector <hectorcruzb5@gmail.com>
2025-06-11 18:16:08 -0600

    commit html, estilos y javaScript

---
+++ Accordion/index.js
@@ -0,0 +1,120 @@
+
+const verInformacion = document.getElementById('verContent-one');
+verInformacion.addEventListener('click', function(event){
+
+    let informacion = document.getElementById('p-one');
+    let cambiarFlecha = document.getElementById('verContent-one');
+
+    if(informacion.style.display === 'none' || informacion.style.display === ''){
+
+        informacion.style.display = 'block';
+        cambiarFlecha.style.transform = 'rotateX(180deg)';
+
+    }else{
+
+        informacion.style.display = 'none';
+        cambiarFlecha.style.transform = 'rotateY(180deg)';
+
+    }
+
+});
+
+
+const verInformacionTwo = document.getElementById('verContent-two');
+verInformacionTwo.addEventListener('click', function(event){
+
+    let informacion = document.getElementById('p-two');
+    let cambiarFlecha = document.getElementById('verContent-two');
+
+    if(informacion.style.display === 'none' || informacion.style.display === ''){
+
+        informacion.style.display = 'block';
+        cambiarFlecha.style.transform = 'rotateX(180deg)';
+
+    }else{
+
+        informacion.style.display = 'none';
+        cambiarFlecha.style.transform = 'rotateY(180deg)';
+
+    }
+});
+
+
+const verInformacionThree = document.getElementById('verContent-three');
+verInformacionThree.addEventListener('click', function(event){
+
+    let informacion = document.getElementById('p-three');
+    let cambiarFlecha = document.getElementById('verContent-three');
+
+    if(informacion.style.display === 'none' || informacion.style.display === ''){
+
+        informacion.style.display = 'block';
+        cambiarFlecha.style.transform = 'rotateX(180deg)';
+
+    }else{
+
+        informacion.style.display = 'none';
+        cambiarFlecha.style.transform = 'rotateY(180deg)';
+
+    }
+});
+
+
+const verInformacionFour = document.getElementById('verContent-four');
+verInformacionFour.addEventListener('click', function(event){
+
+    let informacion = document.getElementById('p-four');
+    let cambiarFlecha = document.getElementById('verContent-four');
+
+    if(informacion.style.display === 'none' || informacion.style.display === ''){
+
+        informacion.style.display = 'block';
+        cambiarFlecha.style.transform = 'rotateX(180deg)';
+
+    }else{
+
+        informacion.style.display = 'none';
+        cambiarFlecha.style.transform = 'rotateY(180deg)';
+
+    }
+});
+
+const verInformacionFive = document.getElementById('verContent-five');
+verInformacionFive.addEventListener('click', function(event){
+
+    let informacion = document.getElementById('p-five');
+    let cambiarFlecha = document.getElementById('verContent-five');
+
+    if(informacion.style.display === 'none' || informacion.style.display === ''){
+
+        informacion.style.display = 'block';
+        cambiarFlecha.style.transform = 'rotateX(180deg)';
+
+    }else{
+
+        informacion.style.display = 'none';
+        cambiarFlecha.style.transform = 'rotateY(180deg)';
+
+    }
+});
+
+
+const verInformacionSix = document.getElementById('verContent-six');
+verInformacionSix.addEventListener('click', function(event){
+
+    let informacion = document.getElementById('p-six');
+    let cambiarFlecha = document.getElementById('verContent-six');
+
+    if(informacion.style.display === 'none' || informacion.style.display === ''){
+
+        informacion.style.display = 'block';
+        cambiarFlecha.style.transform = 'rotateX(180deg)';
+
+    }else{
+
+        informacion.style.display = 'none';
+        cambiarFlecha.style.transform = 'rotateY(180deg)';
+
+    }
+});
+
